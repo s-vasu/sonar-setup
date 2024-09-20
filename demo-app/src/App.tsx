d7a77aad1eb10d848c87e7c3807c7af74c8801e0
s-vasu <vasu.suryavamsham@zemosolabs.com>
2024-09-20 14:20:26 +0530

    project setup and configurations setup

---
+++ demo-app/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Box, Typography } from '@mui/material';
+
+function App() {
+  return (
+    <div className="App">
+      <Box>
+        <Typography>
+          hi
+        </Typography>
+      </Box>
+    </div>
+  );
+}
+
+export default App;
